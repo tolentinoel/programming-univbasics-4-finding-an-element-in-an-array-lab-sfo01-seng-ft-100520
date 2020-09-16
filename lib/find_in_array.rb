904a12469452734dc68be4996c9599992a19da09
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-16 23:44:34 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,9 @@
 def find_element_index(array, value_to_find)
-  i = 0
-  
   if array.include?(value_to_find) 
   idx = array.index( value_to_find)
-end
+  end
 puts idx
 end
 
 name = ["ellaine", "ella", "kim"]
-find_element_index(name, "kim")
+find_element_index(name, "joe")
