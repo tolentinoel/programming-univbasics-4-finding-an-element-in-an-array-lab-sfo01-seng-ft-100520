d2f21fb7dc9f9390ff27b8e7b2c4d5a4940245fb
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-16 23:25:26 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,13 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  index = 0
+  
+  while array.include?(value_to_find) do
+  idx = value_to_find[index]
+  index += 1
+  puts idx
 end
+  idx
+end
+
+name = ["ellaine", "ella", "kim"]
+find_element_index(name, "kim")
