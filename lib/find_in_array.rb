eaf7c281b659cae8901d5deecf39255196ab07bd
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-16 23:32:07 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
-  i = 0
+  i = " "
   
   if array.include?(value_to_find) 
-  idx = #{value_to_find[i]} += 1
+  idx = #{value_to_find[i]} 
   puts idx
 end
 
